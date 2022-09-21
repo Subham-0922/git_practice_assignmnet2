d1cdc6add01891eece8599f52d54a17a5cd78d4b
Subham-0922 <ksubham789@gmail.com>
2022-09-21 09:22:41 +0530

    prime function installed

---
+++ prime.js
@@ -0,0 +1,18 @@
+function primecheck(num){
+let count=1
+for(let i=1;i<=num;i++){
+  if(num%i=0){
+    count++
+  }
+}if(count==2){
+  return true
+}else{
+  return false
+}
+}
+let input=67
+if(primecheck(input)==true){
+  console.log(input+is a prime number")
+}else{
+  console.log(input+"is not a prime number")
+}
