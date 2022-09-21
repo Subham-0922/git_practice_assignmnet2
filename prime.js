af68dba345f6c3e3a0fddaef67fce983027fe75f
Subham-0922 <ksubham789@gmail.com>
2022-09-21 10:57:10 +0530

    fixed

--- prime.js
+++ prime.js
@@ -1,7 +1,7 @@
 function primecheck(num){
 let count=1
 for(let i=1;i<=num;i++){
-  if(num%i=0){
+  if(num%i==0){
     count++
   }
 }if(count==2){
@@ -12,7 +12,7 @@ for(let i=1;i<=num;i++){
 }
 let input=67
 if(primecheck(input)==true){
-  console.log(input+is a prime number")
+  console.log(input+"is a prime number")
 }else{
   console.log(input+"is not a prime number")
 }
